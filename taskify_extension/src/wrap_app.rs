7ff82a859f6158c07f1f5804065628877c0a76ed
Blair HInes <Lubba64@gmail.com>
2026-04-26 22:20:55 -0400

    refactor: Simplify TaskifyExtensionApp::new

--- taskify_extension/src/wrap_app.rs
+++ taskify_extension/src/wrap_app.rs
@@ -2,9 +2,7 @@ pub struct TaskifyExtensionApp;
 
 impl TaskifyExtensionApp {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        #[allow(clippy::allow_attributes, unused_mut)]
-        let mut slf = Self {};
-        slf
+        Self {}
     }
 }
 
